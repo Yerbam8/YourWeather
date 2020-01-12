e5ba8b05619be0d955d4f251bac58a841d9c8716
Kamil Gruda <kamil.gruda94@gmail.com>
2020-01-12 13:24:35 +0100

    First commit

---
+++ src/main/java/pl/sda/pk/YourWeather/YourWeatherApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.pk.YourWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/pk/YourWeather/YourWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.pk.YourWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
